72f259c273e18f548243819bc3f21b12fe03c066
georgeadeishvili <giorgi.adeishvili@gmail.com>
2021-10-15 00:03:40 +0400

    babel env removed

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,31 +1,13 @@
 module.exports = {
-  presets: [
-    'module:metro-react-native-babel-preset',
-    [
-      '@babel/preset-env',
-      {
-        targets: {node: 'current'},
-      },
-    ],
-    '@babel/preset-typescript',
-  ],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        root: ['./src'],
-        extensions: [
-          '.ios.js',
-          '.android.js',
-          '.js',
-          '.ts',
-          '.tsx',
-          '.jsx',
-          '.json',
-        ],
-      },
-    ],
-    ['@babel/plugin-proposal-class-properties', {loose: false}],
-    'react-native-reanimated/plugin',
-  ],
-};
+	presets: ['module:metro-react-native-babel-preset', '@babel/preset-typescript'],
+	plugins: [
+		[
+			'module-resolver',
+			{
+				root: ['./src'],
+				extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.jsx', '.json']
+			}
+		],
+		'react-native-reanimated/plugin'
+	]
+}
